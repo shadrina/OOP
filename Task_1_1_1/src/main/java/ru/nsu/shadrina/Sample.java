5f204476a7408ea1fd7110527e60bf772b6aadd5
Anastasia Shadrina <a.shadrina5@g.nsu.ru>
2023-07-23 23:50:27 +0700

    Add sample task

---
+++ Task_1_1_1/src/main/java/ru/nsu/shadrina/Sample.java
@@ -0,0 +1,11 @@
+package ru.nsu.shadrina;
+
+public class Sample {
+    public static void f() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        f();
+    }
+}
---
+++ Task_1_1_1/src/test/java/ru/nsu/shadrina/SampleTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.shadrina;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void empty() {
+        assertTrue(true);
+    }
+}
